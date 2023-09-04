df4de6ca3f199f2df55ec6042c2d85e6cf7f63a3
Aintdead86 <96647913+antonDedyaev@users.noreply.github.com>
2023-09-04 19:23:24 +0300

    configured the app

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require('path');
+
+const nextConfig = {
+    reactStrictMode: true,
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+        prependData: `@import "variables.scss";`
+    }
+}
 
 module.exports = nextConfig
